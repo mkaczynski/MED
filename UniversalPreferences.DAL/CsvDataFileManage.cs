bf2f614cd828009e3e1344431fbd0737e4be4014
Pawel Sokolowski <pa.sokolowski@gmail.com>
2013-01-09 21:40:55 +0100

    nowe wczytywanie plikow. poczatek

---
+++ UniversalPreferences.DAL/CsvDataFileManage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UniversalPreferences.Common;
+
+namespace UniversalPreferences.DAL
+{
+    public class CsvDataFileManage : IDataManager
+    {
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Row> GetData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dictionary<int, string> GetMappings()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int MinLeftSideIndex()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
